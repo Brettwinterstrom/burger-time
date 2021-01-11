3db68e31a0dad003854a9046e6a25edf0d6ce8f6
Brett Winterstrom <brettwinterstrom@yahoo.com>
2021-01-10 22:13:57 -0800

    final product

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ USE burgers_db;
 
 DROP TABLE IF EXISTS burgers;
 
-CREATE TABLE burger (
+CREATE TABLE burgers (
 id INT NOT NULL AUTO_INCREMENT,
 burger_name VARCHAR (50) NOT NULL,
 devoured BOOLEAN DEFAULT false,
